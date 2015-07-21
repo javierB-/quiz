dc7acb8a2b332a81a7a39d4bef4f2e7f991870e8
Javier Bordonada <javier.bordonada@gmail.com>
2015-07-21 20:18:04 +0200

    Q8 Modificado

--- models/models.js
+++ models/models.js
@@ -1,13 +1,33 @@
+// postgres://bycyznmgiasvqg:EtPMkbuKJtpd0s1Trt2r9LTTkv@ec2-54-83-20-177.compute-1.amazonaws.com:5432/db0ej7b54thi52
 // models/models.js
 var path =require('path');
+
+// Postgres DATABASE_URL = pstgres://user:passwd@host:port/database
+// SALite   DATABASE_URL = sqlite://:@:/
+
+var url = process.env.RATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name   = (url[6]||null);
+var user      = (url[2]||null);
+var pwd       = (url[3]||null);
+var protocol  = (url[1]||null);
+var dialect   = (url[1]||null);
+var port      = (url[5]||null);
+var host      = (url[4]||null);
+var storage = process.env.DATABASE_STORAGE;
 
 //cargar Modelo ORM
 var Sequelize = require('sequelize');
 
 //Usar BBDD SQLite:
 var sequelize = new Sequelize(null, null, null,
-			{dialect: "sqlite", storage: "quiz.sqlite"}
-		    );
+			{dialect: protocol,
+       protocol: protocol,
+       port:  port,
+       host: host,
+       storage:  storage, // solo SQLite (.env)
+       omitNull: true     // solo Postgress
+       }  
+);
 
 // Importar la definicion de la tabla Quiz en quiz.js
 var Quiz = sequelize.import(path.join(__dirname,'quiz'));
@@ -26,6 +46,11 @@ sequelize.sync().success(function() {
 	};
    });
 });
+
+
+
+
+
 
 
 
